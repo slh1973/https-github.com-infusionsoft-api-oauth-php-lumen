ed2f9b7a66f8dd0dafeff84f229502623008ccc8
Jonathan Kressaty <jonathan@vuurr.com>
2015-04-27 13:09:06 -0700

    basic infusionsoft integration via oauth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,31 @@
 */
 
 $app->get('/', function() use ($app) {
-    return $app->welcome();
+	// Setup a new Infusionsoft SDK object
+	$infusionsoft = new \Infusionsoft\Infusionsoft(array(
+		'clientId'     => getenv('INFUSIONSOFT_CLIENT_ID'),
+		'clientSecret' => getenv('INFUSIONSOFT_CLIENT_SECRET'),
+		'redirectUri'  => 'http://local.infusionsoftexample.com/',
+	));
+
+	// If the serialized token is available in the session storage, we tell the SDK
+	// to use that token for subsequent requests.
+	if (Session::has('token')) {
+		$infusionsoft->setToken(unserialize(Session::get('token')));
+	}
+
+	// If we are returning from Infusionsoft we need to exchange the code for an access token.
+	if (Request::has('code') and !$infusionsoft->getToken()) {
+		$infusionsoft->requestAccessToken(Request::get('code'));
+	}
+
+	if ($infusionsoft->getToken()) {
+		// Save the serialized token to the current session for subsequent requests
+		Session::put('token', serialize($infusionsoft->getToken()));
+
+		$infusionsoft->contacts->add(array('FirstName' => 'John', 'LastName' => 'Doe'));
+	}
+	else {
+		echo '<a href="' . $infusionsoft->getAuthorizationUrl() . '">Click here to connect to Infusionsoft</a>';
+	}
 });
