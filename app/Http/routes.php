6b1ab71e6d13cbeb70f024a78abd0326d3f5f7a8
Jonathan Kressaty <jonathan@vuurr.com>
2015-05-21 14:03:22 -0700

    further annotate and restructure routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,11 +17,24 @@ $app->get('/', function() use ($app) {
 	$infusionsoft = new \Infusionsoft\Infusionsoft(array(
 		'clientId'     => getenv('INFUSIONSOFT_CLIENT_ID'),
 		'clientSecret' => getenv('INFUSIONSOFT_CLIENT_SECRET'),
-		'redirectUri'  => 'http://local.infusionsoftexample.com/',
+		'redirectUri'  => getenv('INFUSIONSOFT_REDIRECT_URL'),
 	));
 
-	// If the serialized token is available in the session storage, we tell the SDK
-	// to use that token for subsequent requests.
+	echo '<a href="' . $infusionsoft->getAuthorizationUrl() . '">Click here to connect to Infusionsoft</a>';
+
+});
+
+$app->get('/callback', function() use($app) {
+
+	// Setup a new Infusionsoft SDK object
+	$infusionsoft = new \Infusionsoft\Infusionsoft(array(
+		'clientId'     => getenv('INFUSIONSOFT_CLIENT_ID'),
+		'clientSecret' => getenv('INFUSIONSOFT_CLIENT_SECRET'),
+		'redirectUri'  => getenv('INFUSIONSOFT_REDIRECT_URL'),
+	));
+
+	// If the serialized token is already available in the session storage, we tell the SDK
+	// to use that token for subsequent requests, rather than try and retrieve another one.
 	if (Session::has('token')) {
 		$infusionsoft->setToken(unserialize(Session::get('token')));
 	}
@@ -31,18 +44,20 @@ $app->get('/', function() use ($app) {
 		$infusionsoft->requestAccessToken(Request::get('code'));
 	}
 
+	// NOTE: there's some magic in the step above - the Infusionsoft SDK has not only requested an access token,
+	// but also set the token in the current Infusionsoft object, so there's no need for you to do it.
+
 	if ($infusionsoft->getToken()) {
 		// Save the serialized token to the current session for subsequent requests
 		// NOTE: this can be saved in your database - make sure to serialize the entire token for easy future access
 		Session::put('token', serialize($infusionsoft->getToken()));
 
-		// Now redirect the user to a page that performs some infusionsoft actions
+		// Now redirect the user to a page that performs some Infusionsoft actions
 		return redirect()->to('/contacts');
 	}
-	else {
-		echo '<a href="' . $infusionsoft->getAuthorizationUrl() . '">Click here to connect to Infusionsoft</a>';
-	}
 
+	// something didn't work, so let's go back to the beginning
+	return redirect()->to('/');
 });
 
 $app->get('/contacts', function() use ($app) {
@@ -51,22 +66,23 @@ $app->get('/contacts', function() use ($app) {
 	$infusionsoft = new \Infusionsoft\Infusionsoft(array(
 		'clientId'     => getenv('INFUSIONSOFT_CLIENT_ID'),
 		'clientSecret' => getenv('INFUSIONSOFT_CLIENT_SECRET'),
-		'redirectUri'  => 'http://local.infusionsoftexample.com/',
+		'redirectUri'  => getenv('INFUSIONSOFT_REDIRECT_URL'),
 	));
 
-	// set the token if we have it in storage (in this case, a session)
+	// Set the token if we have it in storage (in this case, a session)
 	$infusionsoft->setToken(unserialize(Session::get('token')));
 
 	try {
-		// retrieve a list of contacts by querying the data service
+		// Retrieve a list of contacts by querying the data service
 		$contacts = $infusionsoft->data->query('Contact', 10, 0, ['FirstName' => 'John'], ['FirstName', 'LastName', 'Email', 'ID'], 'FirstName', true);
 	} catch (\Infusionsoft\TokenExpiredException $e) {
-		// refresh our access token just in case
+		// Refresh our access token since we've thrown a token expired exception
 		$infusionsoft->refreshAccessToken();
-		// we also have to save the new token, since it's now been refreshed
+		// We also have to save the new token, since it's now been refreshed. We serialize the token to ensure
+		// the entire PHP object is saved and not accidentally converted to a string
 		Session::put( 'token', serialize( $infusionsoft->getToken() ) );
 
-		// retrieve the list of contacts again now that we have a new token
+		// Retrieve the list of contacts again now that we have a new token
 		$contacts = $infusionsoft->data->query('Contact', 10, 0, ['FirstName' => 'John'], ['FirstName', 'LastName', 'Email', 'ID'], 'FirstName', true);
 	}
 
